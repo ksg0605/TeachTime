286efd643289e50ebb72c220660b22683a78c203
ksg0605 <ksg0605@gmail.com>
2024-07-18 12:12:05 +0900

    Initial Commit

---
+++ TeachTime/TeachTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeachTimeApp.swift
+//  TeachTime
+//
+//  Created by 김선규 on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TeachTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
